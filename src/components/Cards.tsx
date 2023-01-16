1dbe7fc30ea8d8d5d2992f6e7fdcb5a30843c4f7
Mr_faizan <106084002+faizan-12345@users.noreply.github.com>
2023-01-17 00:30:41 +0500

    changes occurs

---
+++ src/components/Cards.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+// import Single from '../assets/single.png'
+// import Double from '../assets/double.png'
+// import Triple from '../assets/triple.png'
+
+const Cards = () => {
+    return (
+        <div className='w-full py-[10rem] px-4 bg-white'>
+            <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
+                <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+                    <img className='w-20 mx-auto mt-[-3rem] bg-white' src="https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80" alt="/" />
+                    <h2 className='text-2xl font-bold text-center py-8'>Web development</h2>
+                    <p className='text-center text-4xl font-bold'>$149</p>
+                    <div className='text-center font-medium'>
+                        <p className='py-2 border-b mx-8 mt-8'>Lorem Ipsum is simply </p>
+                        <p className='py-2 border-b mx-8'>Lorem Ipsum is simply dummy text of the printing</p>
+                        <p className='py-2 border-b mx-8'>Lorem Ipsum is simply dumm.</p>
+                    </div>
+                    <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+                </div>
+                <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
+                    <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src="https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80" alt="/" />
+                    <h2 className='text-2xl font-bold text-center py-8'>Digital Marketing</h2>
+                    <p className='text-center text-4xl font-bold'>$149</p>
+                    <div className='text-center font-medium'>
+                        <p className='py-2 border-b mx-8 mt-8'>Lorem Ipsum is simply </p>
+                        <p className='py-2 border-b mx-8'>Lorem Ipsum is simply dummy text of the printing</p>
+                        <p className='py-2 border-b mx-8'>Lorem Ipsum is simply dumm.</p>
+                    </div>
+                    <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+                </div>
+                <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+                    <img className='w-20 mx-auto mt-[-3rem] bg-white' src="https://images.unsplash.com/photo-1581456495146-65a71b2c8e52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80" alt="/" />
+                    <h2 className='text-2xl font-bold text-center py-8'>App Development</h2>
+                    <p className='text-center text-4xl font-bold'>$149</p>
+                    <div className='text-center font-medium'>
+                        <p className='py-2 border-b mx-8 mt-8'>Lorem Ipsum is simply </p>
+                        <p className='py-2 border-b mx-8'>Lorem Ipsum is simply dummy text of the printing</p>
+                        <p className='py-2 border-b mx-8'>Lorem Ipsum is simply dumm.</p>
+                    </div>
+                    <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Cards;
